8674dd8ae7dd2560aa7f9c3312145ad489dd65e8
Denis <ivdenvas@gmail.com>
2022-12-07 17:38:54 +0300

    f

---
+++ Task 57/Program.cs
@@ -0,0 +1,62 @@
+﻿// Составить частотный словарь элементов двумерного массива. Частотный 
+// словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+Console.Clear();
+
+int[,] array = GetArray(3, 3, 0, 10);
+int[] res = RepeatElements(array);
+
+PrintArray(array);
+PrintArray1(res);
+
+int[] RepeatElements(int[,] arr)
+{
+    int[] RepeatTable = new int[10];
+    // int sum = 0;
+    for (int i = 0; i < RepeatTable.Length; i++)
+    {
+        int sum = 0;
+        foreach(int var in arr)
+        {
+            if (i == var)
+            {
+                sum++;
+            }
+            RepeatTable[i] = sum;
+        }
+    }
+    return RepeatTable;
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArray1(int[] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        Console.WriteLine($"Цифра {i} выводится {inArray[i]} раз");
+    }
+}
